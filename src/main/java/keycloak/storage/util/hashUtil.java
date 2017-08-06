10b0141b5ed65eeacf295bd002c909bbedad5a08
vasil <andr_vasil@mail.ru>
2017-08-06 21:12:23 +0300

    Рабочая версия

--- src/main/java/keycloak/storage/util/hashUtil.java
+++ src/main/java/keycloak/storage/util/hashUtil.java
@@ -8,8 +8,6 @@ package keycloak.storage.util;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
